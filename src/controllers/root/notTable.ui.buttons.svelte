9c9cbba3042e595ea3a7918697d6667c6361b6e5
Aleksander Repin <alex.repin@mail.ru>
2020-06-22 03:09:11 +0300

    0.2.7 && git push && npm publish

--- src/controllers/root/notTable.ui.buttons.svelte
+++ src/controllers/root/notTable.ui.buttons.svelte
@@ -2,6 +2,12 @@
   export let values = [];
 </script>
 
-{#each values as item (item) }
-<button on:click="{item.action}" class="button is-link is-small">{item.title}</button>
-{/each}
+<div class="buttons has-addons">
+  {#each values as item (item) }
+  {#if item.type === 'danger'}
+  <button on:click="{item.action}" class="button is-small is-danger">{item.title}</button>
+  {:else }
+  <button on:click="{item.action}" class="button is-link is-small">{item.title}</button>
+  {/if}
+  {/each}
+</div>
