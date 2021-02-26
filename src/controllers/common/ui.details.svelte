9c7ee337822bfd2d3cd28cfc7f94b99c9d587aaf
Aleksander Repin <alex.repin@mail.ru>
2021-02-26 20:21:08 +0300

    fix: user profile UI

--- src/controllers/common/ui.details.svelte
+++ src/controllers/common/ui.details.svelte
@@ -7,17 +7,17 @@
 <div class="container mt-2">
   <div class="columns">
     <div class="column title is-5">ID</div>
-    <div class="column">{user.userID}</div>
+    <div class="column subtitle is-5">{user.userID}</div>
   </div>
 
   <div class="columns">
     <div class="column title is-5">Имя пользователя</div>
-    <div class="column">{user.username}</div>
+    <div class="column subtitle is-5">{user.username}</div>
   </div>
 
   <div class="columns">
     <div class="column title is-5">Email</div>
-    <div class="column">
+    <div class="column subtitle is-5">
       {user.email}
       {#if user.emailConfirmed }
       <span class="tag is-success">Подтверждён</span>
@@ -29,7 +29,7 @@
 
   <div class="columns">
     <div class="column title is-5">Телефон</div>
-    <div class="column">
+    <div class="column subtitle is-5">
       {user.telephone}
       {#if user.telephoneConfirmed }
       <span class="tag is-success">Подтверждён</span>
@@ -41,19 +41,19 @@
 
   <div class="columns">
     <div class="column title is-5">Дата создания</div>
-    <div class="column">{user.created}</div>
+    <div class="column subtitle is-5">{user.created}</div>
   </div>
 
   <div class="columns">
     <div class="column title is-5">Роли</div>
-    <div class="column">
+    <div class="column subtitle is-5">
       {user.role.join(', ')}
     </div>
   </div>
 
   <div class="columns">
     <div class="column title is-5">Активен</div>
-    <div class="column">
+    <div class="column subtitle is-5">
       {#if user.active }
       <span class="tag is-success">Активен</span>
       {:else }
@@ -64,12 +64,12 @@
 
   <div class="columns">
     <div class="column title is-5">IP</div>
-    <div class="column">{user.ip}</div>
+    <div class="column subtitle is-5">{user.ip}</div>
   </div>
 
   <div class="columns">
     <div class="column title is-5">Страна</div>
-    <div class="column">{user.country}</div>
+    <div class="column subtitle is-5">{user.country}</div>
   </div>
 
 </div>
--- src/controllers/common/ui.profile.svelte
+++ src/controllers/common/ui.profile.svelte
@@ -25,22 +25,22 @@
   });
 </script>
 
-<h2 class="h2">Пользователь ({user.userID}#{user.username})</h2>
-<div class="container">
+<h2 class="title is-2">Пользователь ({user.userID}#{user.username})</h2>
+<div class="container mt-2">
 
   <div class="columns">
-    <div class="column">ID</div>
-    <div class="column">{user.userID}</div>
+    <div class="column title is-5">ID</div>
+    <div class="column subtitle is-5">{user.userID}</div>
   </div>
 
   <div class="columns">
-    <div class="column">Имя пользователя</div>
-    <div class="column">{user.username}</div>
+    <div class="column title is-5">Имя пользователя</div>
+    <div class="column subtitle is-5">{user.username}</div>
   </div>
 
   <div class="columns">
-    <div class="column">Email</div>
-    <div class="column">
+    <div class="column title is-5">Email</div>
+    <div class="column subtitle is-5">
       {user.email}
       {#if user.emailConfirmed }
       <span class="tag is-success">Подтверждён</span>
@@ -51,8 +51,8 @@
   </div>
 
   <div class="columns">
-    <div class="column">Телефон</div>
-    <div class="column">
+    <div class="column title is-5">Телефон</div>
+    <div class="column subtitle is-5">
       {#if user.telephone && typeof user.telephone !== 'undefined' && user.telephone !== 'undefined'}
       {user.telephone}
       {#if user.telephoneConfirmed }
@@ -67,19 +67,19 @@
   </div>
 
   <div class="columns">
-    <div class="column">Дата создания</div>
-    <div class="column">{user.created}</div>
+    <div class="column title is-5">Дата создания</div>
+    <div class="column subtitle is-5">{user.created}</div>
   </div>
 
   <div class="columns">
-    <div class="column">Роли</div>
-    <div class="column">
+    <div class="column title is-5">Роли</div>
+    <div class="column subtitle is-5">
       <UITag variants={UserCommon.ROLES} items={userRoles} readonly={true} />
     </div>
   </div>
 
   <div class="columns">
-    <div class="column">Активен</div>
+    <div class="column title is-5">Активен</div>
     <div class="column">
       {#if user.active }
       <span class="tag is-success">Активен</span>
@@ -90,18 +90,18 @@
   </div>
 
   <div class="columns">
-    <div class="column">IP</div>
-    <div class="column">{user.ip}</div>
+    <div class="column title is-5">IP</div>
+    <div class="column subtitle is-5">{user.ip}</div>
   </div>
 
   <div class="columns">
-    <div class="column">Страна</div>
-    <div class="column">{user.country}</div>
+    <div class="column title is-5">Страна</div>
+    <div class="column subtitle is-5">{user.country}</div>
   </div>
 
   <div class="columns">
-    <div class="column">Пароль</div>
-    <div class="column">
+    <div class="column title is-5">Пароль</div>
+    <div class="column subtitle is-5">
       <button on:click={goChangePassword} class="button is-small is-warning">Измененить</button>
       <p class="help">Рекомендуется регулярно менять пароль.</p>
     </div>
