018c4b24603ae56473b10cb49d25c08e379cc96d
interrupter <alex.repin@mail.ru>
2023-03-09 21:49:39 +0300

    fix: redirects after login, change form mode after request of OTC to OTC enter form; change: locales/ru OTC code related fixes

--- src/controllers/guest/forms/login.js
+++ src/controllers/guest/forms/login.js
@@ -1,44 +1,49 @@
-import {Frame, notCommon} from 'not-bulma';
-const {notForm} = Frame;
+import { Frame, notCommon } from "not-bulma";
+const { notForm } = Frame;
 
-import ValidatorsLib from '../../common/validators.js';
+import ValidatorsLib from "../../common/validators.js";
 
-export default class LoginForm extends notForm{
-  constructor({data = {}, options = {}}){
-    options.action = 'login';
-    options.model = 'user';
-    super({
-      name: 'Login',
-      options,
-      data
-    });
-    //events after user button actions
-    this.on('submit', e => this.onSubmit(e));
-    this.on('reject', () => {
-      location.href = '/';
-    });
-    this.on('success', () => {
-      location.href = '/dashboard';
-    });
-  }
-
-  getFormValidators(){
-    return notCommon.getApp().getService('nsUser').augmentValidators(ValidatorsLib);
-  }
-
-  async onSubmit(data){
-    try{
-      this.setLoading();
-      const result = await this.getModel('user', data)[`$${this.getFormAction()}`]();
-      //maybe error or success
-      this.processResult(result);
-    }catch(e){
-      //if exactly error
-      this.processResult(e);
-    }finally{
-      //should unlock UI anyway
-      this.resetLoading();
+export default class LoginForm extends notForm {
+    constructor({ data = {}, options = {} }) {
+        options.action = "login";
+        options.model = "user";
+        super({
+            name: "Login",
+            options,
+            data,
+        });
+        //events after user button actions
+        this.on("submit", (e) => this.onSubmit(e));
+        this.on("reject", () => {
+            location.href = "/";
+        });
+        this.on("success", () => {
+            location.href = "/dashboard";
+            location.reload();
+        });
     }
-  }
 
+    getFormValidators() {
+        return notCommon
+            .getApp()
+            .getService("nsUser")
+            .augmentValidators(ValidatorsLib);
+    }
+
+    async onSubmit(data) {
+        try {
+            this.setLoading();
+            const result = await this.getModel("user", data)[
+                `$${this.getFormAction()}`
+            ]();
+            //maybe error or success
+            this.processResult(result);
+        } catch (e) {
+            //if exactly error
+            this.processResult(e);
+        } finally {
+            //should unlock UI anyway
+            this.resetLoading();
+        }
+    }
 }
--- src/controllers/guest/forms/requestLoginCodeOnEmail.js
+++ src/controllers/guest/forms/requestLoginCodeOnEmail.js
@@ -1,41 +1,53 @@
-import {Frame, notCommon} from 'not-bulma';
-const {notForm} = Frame;
+import { Frame, notCommon } from "not-bulma";
+const { notForm } = Frame;
 
-import ValidatorsLib from '../../common/validators.js';
+import ValidatorsLib from "../../common/validators.js";
 
-export default class RequestLoginCodeOnEmailForm extends notForm{
-  constructor({data = {}, options = {}}){
-    options.action = 'requestLoginCodeOnEmail';
-    options.model = 'user';
-    super({
-      name: 'RequestLoginCodeOnEmail',
-      options,
-      data
-    });
-    //events after user button actions
-    this.on('submit', e => this.onSubmit(e));
-    this.on('reject', () => {
-      location.href = '/';
-    });
-  }
-
-  getFormValidators(){
-    return notCommon.getApp().getService('nsUser').augmentValidators(ValidatorsLib);
-  }
-
-  async onSubmit(data){
-    try{
-      this.setLoading();
-      const result = await this.getModel('user', data)[`$${this.getFormAction()}`]();
-      //maybe error or success
-      this.processResult(result);
-    }catch(e){
-      //if exactly error
-      this.processResult(e);
-    }finally{
-      //should unlock UI anyway
-      this.resetLoading();
+export default class RequestLoginCodeOnEmailForm extends notForm {
+    constructor({ data = {}, options = {} }) {
+        options.action = "requestLoginCodeOnEmail";
+        options.model = "user";
+        super({
+            name: "RequestLoginCodeOnEmail",
+            options,
+            data,
+        });
+        //events after user button actions
+        this.on("submit", (e) => this.onSubmit(e));
+        this.on("reject", () => {
+            location.href = "/";
+            location.reload();
+        });
+        this.on("success", () => {
+            if (this.getOptions("changeMode")) {
+                setTimeout(() => {
+                    this.getOptions("changeMode")("loginByCode");
+                }, 1000);
+            }
+        });
     }
-  }
 
+    getFormValidators() {
+        return notCommon
+            .getApp()
+            .getService("nsUser")
+            .augmentValidators(ValidatorsLib);
+    }
+
+    async onSubmit(data) {
+        try {
+            this.setLoading();
+            const result = await this.getModel("user", data)[
+                `$${this.getFormAction()}`
+            ]();
+            //maybe error or success
+            this.processResult(result);
+        } catch (e) {
+            //if exactly error
+            this.processResult(e);
+        } finally {
+            //should unlock UI anyway
+            this.resetLoading();
+        }
+    }
 }
