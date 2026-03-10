6352ae8fc4e82a9ede804a58f22099978a5872b9
Aleksandr Repin <admin@appmon.ru>
2026-03-11 01:11:21 +0300

    fix: ip validator support of multiple ips

--- src/fields/validators/ip.js
+++ src/fields/validators/ip.js
@@ -3,7 +3,14 @@ const { MODULE_NAME } = require("../../const");
 module.exports = [
     {
         validator: (val, { validator }) => {
-            return validator.isIP(val);
+            if (val.indexOf(",") > -1) {
+                return val
+                    .split(",")
+                    .map((ip) => ip.trim())
+                    .every((ip) => validator.isIP(ip));
+            } else {
+                return validator.isIP(val);
+            }
         },
         message: `${MODULE_NAME}:ip_address_is_not_valid`,
     },
